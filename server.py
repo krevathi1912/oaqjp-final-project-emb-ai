c0c1eaa27da95c0894806dde3e59f84b13a8d4df
krevathi1912 <krevathi1912@gmail.com>
2025-06-07 15:32:10 -0400

    Full code

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"This file contains the Flask application for emotion detection."
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +6,20 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_predictor():
+    "Predicts the emotion of a given text using the emotion detector model."
     text_to_analyse = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyse)
-    return_response = "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominanant emotion is {}".format(response['anger'], response['disgust'], response['fear'], response['joy'],response['sadness'], response['dominant_emotion'])
+    return_response = (
+        f"For the given statement, the system response is 'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+        f"'The dominant emotion is: {response['dominant_emotion']}'."
+    )
     return return_response
 
 @app.route("/")
 def render_index_page():
+    "Initializes the index page of the application."
     return render_template('index.html')
 
 if __name__ == "__main__":
